9e955d40d6b71d3b9728dca7361c5f5b14ad4216
RRD <info@enegyner.net>
2025-07-10 09:52:00 -0400

    Desplegando el microservicio ProduccionSolar(2)

--- api/solar-server.mjs
+++ api/solar-server.mjs
@@ -1,36 +1,35 @@
 // 1- SERVIDOR EXPRESS - PUERTO 3010
-import express from "express";
-import cors from "cors";
-import { calcularProduccionSolar } from "../calculations/solar-production.mjs";
+//import express from "express";
+// import cors from "cors";
+// import { calcularProduccionSolar } from "../calculations/solar-production.mjs";
 
-const app = express();
+// const app = express();
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
+// // Middlewares
+// app.use(cors());
+// app.use(express.json());
 
-// 2- RUTAS GET PARA CADA API
+// // 2- RUTAS GET PARA CADA API
 
-//  Api produccion-solar
-app.get("/api/produccion-solar", (req, res) => {
-  console.log("1 - Captando solicitud GET en /api/produccion-solar");
-  res.send(
-    "API Produccion Solar  activa -Usa POST para calcular la produccion-solar de paneles solares."
-  );
-});
-// 3- PROCESADORES PARA CAPTAR POR POST DE CADA API
+// //  Api produccion-solar
+// app.get("/api/produccion-solar", (req, res) => {
+//   console.log("1 - Captando solicitud GET en /api/produccion-solar");
+//   res.send(
+//     "API Produccion Solar  activa -Usa POST para calcular la produccion-solar de paneles solares."
+//   );
+// });
+// // 3- PROCESADORES PARA CAPTAR POR POST DE CADA API
 
-// Importamos produccion-solar
-app.post("/api/produccion-solar", calcularProduccionSolar);
+// // Importamos produccion-solar
+// app.post("/api/produccion-solar", calcularProduccionSolar);
 
-// 4- CODIGO COMUN DEL SERVIDOR
+// // 4- CODIGO COMUN DEL SERVIDOR
 
-// Iniciar servidor
-const PORT = 3010;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`4 - API corriendo en http://localhost:${PORT}`);
-});
-
+// // Iniciar servidor
+// const PORT = 3010;
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`4 - API corriendo en http://localhost:${PORT}`);
+// });
 
 //2- DESPLIEGE EN LA NUBE EN LA PLATAFORMA VERCEL
 /**Eliminar express y app.listen del servidor para convertirlo en una función
@@ -41,7 +40,8 @@ import { calcularProduccionSolar } from "../calculations/solar-production.mjs";
 export default async function handler(req, res) {
   if (req.method === "GET") {
     return res.status(200).json({
-      mensaje: "API Producción Solar activa - Usa POST para calcular la producción solar de paneles"
+      mensaje:
+        "API Producción Solar activa - Usa POST para calcular la producción solar de paneles",
     });
   }
 
